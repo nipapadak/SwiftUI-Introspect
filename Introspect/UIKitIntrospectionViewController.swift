11d2a33fc621c77a47725330c6e9743edc98ebf4
Nikolaos Papadakis <nipapadak@gmail.com>
2020-05-12 16:56:21 -0400

    call customization at start

--- Introspect/UIKitIntrospectionViewController.swift
+++ Introspect/UIKitIntrospectionViewController.swift
@@ -35,6 +35,11 @@ public struct UIKitIntrospectionViewController<TargetViewControllerType: UIViewC
         let viewController = IntrospectionUIViewController()
         viewController.accessibilityLabel = "IntrospectionUIViewController<\(TargetViewControllerType.self)>"
         viewController.view.accessibilityLabel = "IntrospectionUIView<\(TargetViewControllerType.self)>"
+        
+        if let targetView = self.selector(viewController) {
+            self.customize(targetView)
+        }
+        
         return viewController
     }
     
